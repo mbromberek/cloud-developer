454c33cce382aa16671f9af19ef22cf3758b5822
mbromberek <mikebromberek@gmail.com>
2019-10-20 13:06:56 -0500

    implement authentication

--- exercises/lesson-5/client/src/config.ts
+++ exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://74m59i7h22.execute-api.us-east-2.amazonaws.com/dev/'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-5-85zgsg.auth0.com',
+  clientId: 'T686ScdzpvQwUIA43d7ybezz0gQDaHLE',
   callbackUrl: 'http://localhost:3000/callback'
 }
--- exercises/lesson-5/starter-code/src/lambda/auth/auth0Authorizer.ts
+++ exercises/lesson-5/starter-code/src/lambda/auth/auth0Authorizer.ts
@@ -50,11 +50,13 @@ export const handler = middy(async (event: CustomAuthorizerEvent, context): Prom
 })
 
 function verifyToken(authHeader: string, secret: string): JwtToken {
-  if (!authHeader)
+  if (!authHeader){
     throw new Error('No authentication header')
+  }
 
-  if (!authHeader.toLowerCase().startsWith('bearer '))
+  if (!authHeader.toLowerCase().startsWith('bearer ')){
     throw new Error('Invalid authentication header')
+  }
 
   const split = authHeader.split(' ')
   const token = split[1]
