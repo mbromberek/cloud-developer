97faba0b051ea2ad17125cab69a7369bcfdbec64
mbromberek <mikebromberek@gmail.com>
2019-10-26 21:53:52 -0500

    Got Update Todo working

--- project/c4-final-project/backend/src/lambda/http/updateTodo.ts
+++ project/c4-final-project/backend/src/lambda/http/updateTodo.ts
@@ -1,8 +1,13 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { createLogger } from '../../utils/logger'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+// const todosIdIndex = process.env.TODOS_ID_INDEX
 const logger = createLogger('updateToDo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -11,6 +16,51 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
   logger.info('handler', updatedTodo)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  // Check if passed ID exists
+  const result = await docClient.query({
+      TableName : todosTable,
+//       IndexName : todosIdIndex,
+      KeyConditionExpression: 'todoId = :todoId',
+      ExpressionAttributeValues: {
+          ':todoId': todoId
+      }
+  }).promise()
+  
+  //If no records returned, return a 404 not found error
+  if (result.Count == 0){
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: ''
+    }
+  
+  }
+  
+  await docClient.update({
+      TableName: todosTable,
+      Key:{
+        "todoId": todoId
+      },
+      UpdateExpression: "set #nm = :todoName, dueDate = :dueDate, done = :done",
+      ExpressionAttributeValues: {
+          ":todoName": updatedTodo.name,
+          ':dueDate': updatedTodo.dueDate,
+          ":done": updatedTodo.done   
+      },
+      ExpressionAttributeNames: {
+    //Using expression for name field since name is reserved word for DynamoDB on Updates
+        "#nm": "name"
+      },
+      ReturnValues: "UPDATED_NEW"
+  }).promise()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: ''
+  }
 }
