7aa7094586aaf7ed60ac1f01a5045a4984414663
mbromberek <mikebromberek@gmail.com>
2019-10-27 06:09:14 -0500

    Added Delete functionality

--- project/c4-final-project/backend/src/lambda/http/deleteTodo.ts
+++ project/c4-final-project/backend/src/lambda/http/deleteTodo.ts
@@ -1,13 +1,32 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
 import { createLogger } from '../../utils/logger'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 const logger = createLogger('deleteToDo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  //Get ToDo ID from URL path
   const todoId = event.pathParameters.todoId
-  logger.info('DeleteToDo', todoId)
+  logger.info('URL Parameters', {'todo': todoId)
+  
+  //Perform delete
+  await docClient.delete({
+      TableName: todosTable,
+      Key:{
+        "todoId": todoId
+      }
+  }).promise()
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: ''
+  }
+
 }
