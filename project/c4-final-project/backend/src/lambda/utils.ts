dccb835e1eeac922fe537d369d5daf6a376bfd8c
mbromberek <mikebromberek@gmail.com>
2019-10-27 11:35:04 -0500

    Added todoExists function to check if todo exists, can now be used by multiple functions.

--- project/c4-final-project/backend/src/lambda/utils.ts
+++ project/c4-final-project/backend/src/lambda/utils.ts
@@ -1,6 +1,11 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { parseUserId } from "../auth/utils";
 
+import * as AWS  from 'aws-sdk'
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
+
 /**
  * Get a user id from an API Gateway event
  * @param event an event from API Gateway
@@ -13,4 +18,22 @@ export function getUserId(event: APIGatewayProxyEvent): string {
   const jwtToken = split[1]
 
   return parseUserId(jwtToken)
+}
+
+/*
+  Check if Todo exists 
+  Returns: True if Todo ID exists in database
+           False if Todo ID is not found in database
+*/
+export async function todoExists (todoId: string){
+  // Check if passed ID exists
+  const result = await docClient.query({
+      TableName : todosTable,
+      KeyConditionExpression: 'todoId = :todoId',
+      ExpressionAttributeValues: {
+          ':todoId': todoId
+      }
+  }).promise()
+  
+  return (result.Count >0)
 }
