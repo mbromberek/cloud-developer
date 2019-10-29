f0dcc146267b3c386e7bd3ab8561ac0aefb52e5f
mbromberek <mikebromberek@gmail.com>
2019-10-28 21:08:43 -0500

    Added check for user ID

--- project/c4-final-project/backend/src/lambda/http/getTodos.ts
+++ project/c4-final-project/backend/src/lambda/http/getTodos.ts
@@ -2,7 +2,7 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import * as AWS  from 'aws-sdk'
-
+import { parseUserId } from '../../auth/utils'
 import { createLogger } from '../../utils/logger'
 const logger = createLogger('getTodos')
 
@@ -13,10 +13,20 @@ const todosTable = process.env.TODOS_TABLE
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.info('handler', event)
   
-  
+  var userId = 'default'  
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  if (split.length > 1){
+    const jwtToken = split[1]
+    userId = parseUserId(jwtToken)
+  }
+  logger.info('handler', {'userId': userId})
+
 
   const result = await docClient.scan({
-    TableName: todosTable
+    TableName: todosTable,
+    FilterExpression: "userId = :userId",
+    ExpressionAttributeValues: {':userId': userId}
   }).promise()
 
   const items = result.Items
